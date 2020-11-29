c9cf554be768da5d5690dec68fd18a1198a11a68
Song Seungeun <grayandjj@gmail.com>
2020-11-29 16:14:38 +0900

    fix layout flex, fav icon

--- bizmemory/js/app.js
+++ bizmemory/js/app.js
@@ -2,11 +2,12 @@ let list = [
   {
     id: 1,
     name: "홍길동",
-    company: "가나다주식회사",
+    company: "가나다",
     devision: "UX디자인팀",
     position: "인턴",
     email: "abc@gmail.com",
     mobile: "010-1234-5678",
+    fav: false,
   },
 ];
 let item = {};
@@ -25,21 +26,26 @@ const render = () => {
 
   list.map((namecard) => {
     html += `
-    <li id="1" class="namecard color1">
-      <div class="namecardInfo">
-        <span class="cardName">${namecard.name}</span>
-        <span class="cardMobile">${namecard.mobile}</span>
-        <span class="cardEmail">${namecard.email}</span>
-      </div>
-      <div class="namecardCompany">
-        <span class="cardCompany">${namecard.company}</span>
-        <span class="cardDivision">${namecard.devision}</span>
-        <span class="cardPosition">${namecard.position}</span>
-      </div>
-      <img class="favoriteBtn far" src="./img/fav-icon.png">
-      <img class="favoriteBtn fas" src="./img/fav-icon-on.png">
-      <img class="deleteBtn" src="./img/close-btn.png">
-    </li>`;
+          <li id="1" class="namecard color3">
+            <div class="namecardWrapper">
+              <div class="namecardInfo">
+                <span class="cardName">${namecard.name}</span>
+                <span class="cardMobile">${namecard.mobile}</span>
+                <span class="cardEmail">${namecard.email}</span>
+              </div>
+              <div class="namecardCompany">
+                <span class="cardCompany">${namecard.company}</span>
+                <span class="cardDivision">${namecard.devision}</span>
+                <span class="cardPosition">${namecard.position}</span>
+              </div>
+            </div>
+            <div class="namecardIcon">
+              <img class="favoriteBtn" src="./img/fav-icon${
+                namecard.fav ? "-on" : ""
+              }.png">
+              <img class="deleteBtn" src="./img/close-btn.png">
+            </div>
+          </li>`;
   });
 
   $cardList.innerHTML = html;
